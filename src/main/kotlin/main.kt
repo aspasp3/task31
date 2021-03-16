d576f239deba90cb9a70e80e629bae9455046301
aspasp3 <79167925+aspasp3@users.noreply.github.com>
2021-03-16 20:07:11 +0300

    second commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,29 +1,30 @@
 fun main() {
-    val howLngAgo = 60*60*2
+    val howLngAgo:Int = 60*21
     println("result: " + agoToText(howLngAgo))
 }
 fun agoToText(seconds:Int) : String{
-    val result:String = when {
-        (seconds>0) && (seconds<=60) -> "bil tolko chto"
-        //(seconds>60) && (seconds<=60*60) -> "x минут назад"
-        //(seconds>(60*60+1)) && (seconds<=(24*60*60)) -> "x часов назад"
-        (seconds>60) && (seconds<=60*60) -> {
-            Num2word(seconds/60, "minutu", "minuti", "minut")
+    val result:String = when(seconds) {
+        in 0..60 -> "bil tolko chto"
+        in 61..60*60 -> {
+            //numToWord(seconds/60, "minutu", "minuti", "minut")
+            minutes(seconds/60)
         }
-        (seconds>60*60+1) && (seconds<=24*60*60) -> {
-            Num2word(seconds/(60*60), "chas", "chasa", "chasov")
+        in 60*60+1..24*60*60 -> {
+            //numToWord(seconds/60*60, "chas", "chasa", "chasov")
+            hours(seconds/60*60)
         }
-        (seconds>(24*60*60)) && (seconds<=(24*60*60*2)) -> "bil segodna"
-        (seconds>(24*60*60*2+1)) && (seconds<=(24*60*60*3)) -> "bil vchera"
-        (seconds>(24*60*60*3+1)) -> "bil davno"
-        else -> ""
+        in 24*60*60..24*60*60*2 -> "bil segodna"
+        in 24*60*60*2+1..24*60*60*3 -> "bil vchera"
+        //(seconds>(24*60*60*3+1)) -> "bil davno"
+        else -> "bil davno"
+
     }
     return result
 }
 
-fun Num2word(scnds:Int, word1:String, word2:String, word3:String):String {
+fun numToWord(scnds:Int, word1:String, word2:String, word3:String):String {
     var num = scnds % 100;
-    if (num > 19) {
+    if ((num > 19)||(num<=10)) {
         num = num % 10;
     }
     val result = when(num){
@@ -32,4 +33,11 @@ fun Num2word(scnds:Int, word1:String, word2:String, word3:String):String {
         else ->  "bil $scnds $word3 nazad"
     }
     return result
+}
+
+fun minutes(scnds:Int):String{
+    return numToWord(scnds,"minutu","minuti","minut")
+}
+fun hours(scnds:Int):String{
+    return numToWord(scnds,"chas","chasa","chasov")
 }
